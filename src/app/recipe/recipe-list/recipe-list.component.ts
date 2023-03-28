b36f3ad3020077d47e349647dbfd7b863b47c221
Alisson Zampietro <alisson.zampietro@smooved.be>
2023-03-28 23:41:52 +0100

    fix: property propagation

--- src/app/recipe/recipe-list/recipe-list.component.ts
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -8,8 +8,16 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent {
 
+  @Output() recipeItem = new EventEmitter<Recipe>();
+  
   public recipes: Recipe[] = [
     new Recipe('Chocolate Cake', 'Simple chocolate cake', 'https://thestayathomechef.com/wp-content/uploads/2016/06/The-Most-Amazing-Chocolate-Cake-Thumbnail.jpg'),
     new Recipe('Strawberry Cake', 'Strawberry cake', 'https://thestayathomechef.com/wp-content/uploads/2016/06/The-Most-Amazing-Chocolate-Cake-Thumbnail.jpg'),
   ];
+
+  onRecipeSelected(recipe: Recipe):void {
+    this.recipeItem.emit(recipe);
+  }
+
+
 }
