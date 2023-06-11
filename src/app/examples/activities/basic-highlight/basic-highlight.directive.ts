9defb402fadf70195f13fcc2d31f947c0ce04241
Alisson Zampietro <alisson.zampietro@smooved.be>
2023-06-11 22:17:04 +0100

    feat: testing host binding directive

--- src/app/examples/activities/basic-highlight/basic-highlight.directive.ts
+++ src/app/examples/activities/basic-highlight/basic-highlight.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from "@angular/core";
+import { Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2 } from "@angular/core";
 
 @Directive({
     selector: '[appHighLight]'
@@ -7,8 +7,17 @@ import { Directive, ElementRef, OnInit, Renderer2 } from "@angular/core";
 export class BasicHighlightDirective implements OnInit {
     constructor(private elementRef: ElementRef, private render: Renderer2) {}
 
+    @HostBinding('style.color') textColor: string;
+
     ngOnInit(): void {
         this.render.setStyle(this.elementRef.nativeElement, 'color', '#fff');
         this.elementRef.nativeElement.style.backgroundColor = '#16921c';
     }
+
+    @HostListener('dblclick')
+    public action() {
+        this.textColor = 'pink';
+    }
+
+
 }
--- src/app/examples/activities/better-highlight/better.highlight.directive.ts
+++ src/app/examples/activities/better-highlight/better.highlight.directive.ts
@@ -1,10 +1,13 @@
-import {Directive, ElementRef, HostListener, OnInit, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2} from '@angular/core';
 
 @Directive({
     selector: '[appBetterHighlight]'
 })
 
 export class BetterHighlightDirective implements OnInit{
+
+    // we can pass a string defining to which property of the host element we want to bind
+    @HostBinding('style.background') backgroundColor: string;
 
     @HostListener('mouseenter') 
     public mouseOver(data: Event) {
@@ -13,13 +16,15 @@ export class BetterHighlightDirective implements OnInit{
 
     @HostListener('mouseleave') 
     public mouseLeave(data: Event) {
-        this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', '#0008ff');
+        // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', '#0008ff');
+        this.backgroundColor = 'orange';
         this.renderer.setStyle(this.elementRef.nativeElement, 'color', '#fff');
     }
 
     @HostListener('mousedown') 
     public mouseDown(data: Event) {
-        this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', '#b75b00');
+        // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', '#b75b00');
+        this.backgroundColor = 'blue';
     }
 
     constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
